5c5fe3949bf1d44a2bb5ee025db3f91bf0756d5c
howserstrousers <fritzcharleshowser@gmail.com>
2016-11-28 13:59:59 -0800

    contentless framework

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+ 
+ $( document ).ready(function() {
+
+   	$(".drop").click(function(){
+    	$(".about").toggle();
+	});
+ 	
+ 	$(".drop").click(function(){
+    	$(".wrapper").toggle();
+	});
+
+});
+ 
+};
